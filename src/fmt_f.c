148ce57792cb427dded24e2948bef08a63cf52e7
2ne1ugly <chattermin@gmail.com>
2019-04-26 17:51:21 -0700

    prototype double represent

--- src/fmt_f.c
+++ src/fmt_f.c
@@ -12,13 +12,20 @@
 
 #include "ft_printf_int.h"
 
+void	calculate_fraction(double val)
+{
+
+}
+
 //solve this one later
 void	fmt_f(t_vec *vec, t_opt *opt, va_list *arg)
 {
-	double	val;
-	char	*out;
+	long double	val;
+	char		*out;
 
-	(void)opt;
-	val = va_arg(*arg, double);
+	if (opt->length == 8)
+		val = va_arg(*arg, long double);
+	else
+		val = va_arg(*arg, long double);
 	push_back_str(vec, out, ft_strlen(out));
 }
--- test.c
+++ test.c
@@ -11,8 +11,59 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, char **argv)
+int		get_bit_double(double *val, int n)
 {
-	printf(argv[1], atoi(argv[2]));
+	char *ptr;
+
+	ptr = val;
+	ptr = &ptr[n / 8];
+	return (*ptr >> (n % 8)) & 0b1;
+}
+
+long long	calc_fraction_bit_double(double *val)
+{
+	long long	out;
+	int		i;
+
+	out = 1;
+	i = 51;
+	while(i >= 0)
+	{
+		out <<= 1;
+		out += get_bit_double(val, i);
+		i--;
+	}
+	printf("%d\n", out);
+	if (get_bit_double(val, 63))
+		out *= -1;
+	return (out);
+}
+
+double		calc_exp_bit_double(double *val)
+{
+	int	out;
+	int	i;
+
+	out = 0;
+	i = 62;
+	while (i >= 52)
+	{
+		out *= 2;
+		out += get_bit_double(val, i);
+		printf("%d\n", get_bit_double(val, i));
+		i--;
+	}
+	printf("%d\n",out);
+	return (pow(2, out - 1023 - 52));
+}
+
+int main()
+{
+	double val;
+
+	val = 3.14;
+	printf("%f\n", val);
+	printf("%f\n", calc_exp_bit_double(&val) * calc_fraction_bit_double(&val));
 }
